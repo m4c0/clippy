7effb9151c9d152670f7dfd69f5658b9fd9097dd
Eduardo Costa <m4c0@users.noreply.github.com>
2025-01-18 12:44:22 +0000

    yay, paperclips!

--- poc.cpp
+++ poc.cpp
@@ -3,6 +3,19 @@
 import getch;
 import print;
 
+static int g_paperclips;
+
+static void cycle() {
+  putln("Press P to create a paperclip");
+  if (getch() != 'p') return;
+
+  g_paperclips++;
+
+  putln();
+  putln(g_paperclips, " paperclips");
+  putln();
+}
+
 int main() {
-  while (true) putln(getch());
+  while (true) cycle(); 
 }

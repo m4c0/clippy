784738ccaae9a0755720df0ac79dfeb353bcb984
Eduardo Costa <m4c0@users.noreply.github.com>
2025-01-18 13:17:45 +0000

    tweaks + autoclipper buying

--- poc.cpp
+++ poc.cpp
@@ -5,33 +5,49 @@ import rng;
 
 typedef int numba;
 
-static numba g_wire_spool = 100;
-static numba g_clips_per_box = 10;
-static numba g_cost_per_box = 10;
-static numba g_cost_per_spool = 10;
+static numba g_wire_spool = 1000;
+static numba g_clips_per_box = 1;
+static numba g_cost_per_box = 25;
+static numba g_cost_per_spool = 1500;
+static numba g_autoclip_cost = 0;
+static numba g_demand = 32;
 
 static numba g_paperclips = 0;
 static numba g_wire = g_wire_spool;
 static numba g_funds = 0;
+static numba g_autoclips = 0;
+
+static numba g_autoclip_min_funds = 500;
+
+static bool autoclippers() { return g_autoclip_cost != 0; }
 
 static void draw() {
   putln();
-  putln("Paperclips: ", g_paperclips);
-  putln("Wire:       ", g_wire, " units");
-  putln("Funds:      ", g_funds, " dindins");
+  putln("Paperclips:   ", g_paperclips);
+  putln("Wire:         ", g_wire, " units");
+  putln("Funds:        ", g_funds, " dindins");
+  putln("Autoclippers: ", g_autoclips);
   putln();
   putln("Cost of a paperclip box: ", g_cost_per_box);
   putln("Cost of a wire spool:    ", g_cost_per_spool);
   putln();
   putln("Press P to create a paperclip");
   putln("Press W to buy a wire spool");
+  if (autoclippers()) {
+    putln("Press A to buy an autoclipper");
+  }
 }
 
 static void sell() {
   if (g_paperclips < g_clips_per_box) return;
-  if (rng::randf() > 0.5) return;
+  if (rng::rand(100) < g_demand) return;
   g_paperclips -= g_clips_per_box;
   g_funds += g_cost_per_box;
+
+  if (!autoclippers() && g_funds > g_autoclip_min_funds) {
+    g_autoclip_cost = g_autoclip_min_funds;
+    putln("Autoclippers enabled");
+  }
 }
 
 static void make_paperclip() {

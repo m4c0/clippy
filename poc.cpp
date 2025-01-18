999f590da2cccbd26fa2abdbb318add6e11ed71d
Eduardo Costa <m4c0@users.noreply.github.com>
2025-01-18 12:58:13 +0000

    selling paperclips, buying spools

--- poc.cpp
+++ poc.cpp
@@ -1,17 +1,30 @@
 #pragma leco tool
 import getch;
 import print;
+import rng;
 
 typedef int numba;
 
-static numba g_paperclips;
-static numba g_wire = 1000;
+static numba g_wire_spool = 10;
+
+static numba g_paperclips = 0;
+static numba g_wire = g_wire_spool;
+static numba g_funds = 0;
 
 static void draw() {
   putln();
-  putln(g_paperclips, " paperclips");
-  putln(g_wire, " wire");
+  putln("Paperclips: ", g_paperclips);
+  putln("Wire: ", g_wire, " units");
+  putln("Funds: ", g_funds, " dindins");
   putln("Press P to create a paperclip");
+  putln("Press W to buy a wire spool");
+}
+
+static void sell() {
+  if (g_paperclips == 0) return;
+  if (rng::randf() > 0.5) return;
+  g_paperclips -= 1;
+  g_funds += 1;
 }
 
 static void make_paperclip() {
@@ -20,9 +33,18 @@ static void make_paperclip() {
   g_paperclips++;
 }
 
+static void buy_spool() {
+  if (g_funds == 0) return;
+  g_wire += g_wire_spool;
+  g_funds--;
+}
+
 static void process(char input) {
+  sell();
+
   switch (input) {
     case 'p': make_paperclip(); break;
+    case 'w': buy_spool(); break;
     default:  break;
   }
 }

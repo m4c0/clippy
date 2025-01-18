8a6ef627b165d69fa48b8857ae522fffbd5f2664
Eduardo Costa <m4c0@users.noreply.github.com>
2025-01-18 12:51:13 +0000

    wires

--- poc.cpp
+++ poc.cpp
@@ -1,22 +1,29 @@
 #pragma leco tool
-
 import getch;
 import print;
 
-static int g_paperclips;
+typedef int numba;
+
+static numba g_paperclips;
+static numba g_wire = 1000;
 
 static void draw() {
   putln();
   putln(g_paperclips, " paperclips");
+  putln(g_wire, " wire");
   putln("Press P to create a paperclip");
+}
+
+static void make_paperclip() {
+  if (g_wire == 0) return;
+  g_wire--;
+  g_paperclips++;
 }
 
 static void process(char input) {
   switch (input) {
-    case 'p':
-      g_paperclips++;
-      break;
-    default: break;
+    case 'p': make_paperclip(); break;
+    default:  break;
   }
 }
 
